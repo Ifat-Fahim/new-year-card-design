a10951761d829ac455422e603952c5d0554c0635
shu-vro <shuvra1078@gmail.com>
2021-07-13 12:05:47 +0600

    Images Uploaded for markup.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Animation from "./components/Animation";
 // DO NOT DELETE THIS LINE. I HAVE IMPORTED ALL THE ESSENTIAL IMAGES SO THAT THEY MIGHT LOAD FASTER.
 import EarthTexture from "./img/earth.jpg";
 import SpaceTexture from "./img/space.jpg";
-import NormalEarthTexture from "./img/earth_normal_map.png";
+import NormalEarthTexture from "./img/earth_normal_map.jpg";
 
 let images = {
     EarthTexture: EarthTexture,
